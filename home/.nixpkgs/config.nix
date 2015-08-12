4f3bbf20fcc863959551b067e8ef918269e9dca7
Robin Bate Boerop <me@robinbb.com>
2015-08-12 11:01:45 -0700

    Comment out old-style Haskell overrides for Nix

--- home/.nixpkgs/config.nix
+++ home/.nixpkgs/config.nix
@@ -15,20 +15,20 @@
 #     };
 #   };
 
-  packageOverrides = pkgs: rec {
-     haskellPackages = pkgs.recurseIntoAttrs (pkgs.haskellPackages.override {
-        extension = self : super :
-           let callPackage = self.callPackage;
-            in rec {
-                 genericDeriving_1_6_3 = callPackage ~/.nixpkgs/generic-deriving-1.6.3.nix {} ;
-                 text_1_1_1_3 = callPackage ~/.nixpkgs/text-1.1.1.3.nix {} ;
-                 lens_3_10_3 = callPackage ~/.nixpkgs/lens-3.10.3.nix {
-                    genericDeriving = genericDeriving_1_6_3 ;
-                    text = text_1_1_1_3 ;
-                 } ;
-                 # prettyCompact = callPackage ./local/pretty-compact.nix {};
-               } ;
-     }) ;
-  } ;
+#   packageOverrides = pkgs: rec {
+#      haskellPackages = pkgs.recurseIntoAttrs (pkgs.haskellPackages.override {
+#         extension = self : super :
+#            let callPackage = self.callPackage;
+#             in rec {
+#                  genericDeriving_1_6_3 = callPackage ~/.nixpkgs/generic-deriving-1.6.3.nix {} ;
+#                  text_1_1_1_3 = callPackage ~/.nixpkgs/text-1.1.1.3.nix {} ;
+#                  lens_3_10_3 = callPackage ~/.nixpkgs/lens-3.10.3.nix {
+#                     genericDeriving = genericDeriving_1_6_3 ;
+#                     text = text_1_1_1_3 ;
+#                  } ;
+#                  # prettyCompact = callPackage ./local/pretty-compact.nix {};
+#                } ;
+#      }) ;
+#   } ;
 
 }
