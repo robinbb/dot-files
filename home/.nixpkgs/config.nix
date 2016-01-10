8e4cc93fa98e5409b25a56ba2afed69fb25219c2
Robin Bate Boerop <me@robinbb.com>
2016-01-10 02:37:15 -0400

    Use pure Darwin build for .nixpkgs

--- home/.nixpkgs/config.nix
+++ home/.nixpkgs/config.nix
@@ -1,6 +1,11 @@
 { allowBroken = true ;
   allowUnfree = true ;
 
+  replaceStdenv = { pkgs }: pkgs.allStdenvs.stdenvDarwinPure;
+}
+
+# Old stuff:
+#
 #   packageOverrides = pkgs: rec {
 #     haskellPackages = with pkgs.haskellPackages; pkgs.haskellPackages // rec {
 #       packageName = callPackage ./haskell/package-name.nix {};
@@ -15,20 +20,18 @@
 #     };
 #   };
 
-  packageOverrides = pkgs: rec {
-     haskellPackages = pkgs.recurseIntoAttrs (pkgs.haskellPackages.override {
-        extension = self : super :
-           let callPackage = self.callPackage;
-            in rec {
-                 genericDeriving_1_6_3 = callPackage ~/.nixpkgs/generic-deriving-1.6.3.nix {} ;
-                 text_1_1_1_3 = callPackage ~/.nixpkgs/text-1.1.1.3.nix {} ;
-                 lens_3_10_3 = callPackage ~/.nixpkgs/lens-3.10.3.nix {
-                    genericDeriving = genericDeriving_1_6_3 ;
-                    text = text_1_1_1_3 ;
-                 } ;
-                 # prettyCompact = callPackage ./local/pretty-compact.nix {};
-               } ;
-     }) ;
-  } ;
-
-}
+#   packageOverrides = pkgs: rec {
+#      haskellPackages = pkgs.recurseIntoAttrs (pkgs.haskellPackages.override {
+#         extension = self : super :
+#            let callPackage = self.callPackage;
+#             in rec {
+#                  genericDeriving_1_6_3 = callPackage ~/.nixpkgs/generic-deriving-1.6.3.nix {} ;
+#                  text_1_1_1_3 = callPackage ~/.nixpkgs/text-1.1.1.3.nix {} ;
+#                  lens_3_10_3 = callPackage ~/.nixpkgs/lens-3.10.3.nix {
+#                     genericDeriving = genericDeriving_1_6_3 ;
+#                     text = text_1_1_1_3 ;
+#                  } ;
+#                  # prettyCompact = callPackage ./local/pretty-compact.nix {};
+#                } ;
+#      }) ;
+#   } ;
